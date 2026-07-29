2ebea8ad47bfb42e63a0d8c3e0cec3ed0022f20e
Busola <busolaokemoney@gmail.com>
2026-07-30 00:38:26 +0100

    style: adjust footer image size and refactor formatting for consistency

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { siteConfig } from '~/config/site'
+import { siteConfig } from "~/config/site";
 
 interface FooterProps {
   onImageClick?: (src: string) => void;
@@ -15,11 +15,13 @@ const props = withDefaults(defineProps<FooterProps>(), {
   person1: siteConfig.couple.person1,
   person2: siteConfig.couple.person2,
   wishlistUrl: siteConfig.wishlistUrl,
-})
+});
 </script>
 
 <template>
-  <footer class="bg-linen-white min-h-screen w-full flex flex-col items-center justify-center p-6 border-t border-amber-gold/15 text-center relative">
+  <footer
+    class="bg-linen-white min-h-screen w-full flex flex-col items-center justify-center p-6 border-t border-amber-gold/15 text-center relative"
+  >
     <FadeInSection variant="scale-in" :distance="18">
       <div class="max-w-md mx-auto space-y-4 motion-stagger">
         <p class="text-4xl text-deep-espresso font-semibold font-display-cinzel">
@@ -29,17 +31,12 @@ const props = withDefaults(defineProps<FooterProps>(), {
           Thank you for being part of our story. We can't wait to celebrate our union with you.
         </p>
         <div
-          class="relative w-[150px] aspect-square mx-auto bg-white p-2 pb-6 rounded shadow border border-deep-espresso/5 rotate-3 scale-90 select-none motion-lift"
+          class="relative w-[250px] aspect-square mx-auto bg-white p-2 pb-6 rounded shadow border border-deep-espresso/5 rotate-3 scale-90 select-none motion-lift"
           :class="onImageClick && couplesPhoto ? 'cursor-zoom-in' : ''"
           @click="couplesPhoto && onImageClick?.(couplesPhoto)"
         >
           <div class="relative w-full h-full overflow-hidden bg-deep-espresso/5 rounded-sm">
-            <img
-              v-if="couplesPhoto"
-              :src="couplesPhoto"
-              alt="Memory"
-              class="img-fill"
-            />
+            <img v-if="couplesPhoto" :src="couplesPhoto" alt="Memory" class="img-fill" />
           </div>
         </div>
         <p class="font-body text-xs text-deep-espresso/60 pt-4">
